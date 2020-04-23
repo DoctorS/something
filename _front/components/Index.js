c747e7d2d8e0ccb12f039b1a850a17c10f796bd8
DoctorS <nls.@ya.ru>
2020-04-23 16:39:31 +0300

    aggregation

--- _front/components/Index.js
+++ _front/components/Index.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 
+let agr = []
+for (let i = 0, n = 20; i < n; i++) agr.push('sum')
+
 class Main extends Component {
   constructor(props) {
     super(props)
-    this.state = { data: [] }
+    this.state = { data: [], agr }
   }
 
   componentDidMount() {
@@ -14,7 +17,7 @@ class Main extends Component {
         .post('/get-data')
         .then((d) => this.setState({ data: d.data }))
         .catch(console.log)
-    }, 1000)
+    }, 333)
   }
 
   getBG(v) {
@@ -30,6 +33,48 @@ class Main extends Component {
 
   getRow(e, n) {
     return <div style={{ background: this.getBG(e[`Param${n}`]), color: this.getColor(e[`Param${n}`]) }}>{e[`Param${n}`]}</div>
+  }
+
+  aggregation(e, n) {
+    let agr = this.state.agr
+    agr[n - 1] = e.target.value
+  }
+
+  getAgrData(n) {
+    let typeAgr = this.state.agr[n - 1]
+    let a = []
+    switch (typeAgr) {
+      case 'sum':
+        a = this.state.data.map((e) => e[`Param${n}`])
+        return a.length ? a.reduce((a, b) => a + b).toFixed(2) : ''
+      case 'max':
+        a = this.state.data.map((e) => e[`Param${n}`])
+        return a.length ? Math.max.apply(null, a).toFixed(2) : ''
+      case 'min':
+        a = this.state.data.map((e) => e[`Param${n}`])
+        return a.length ? Math.min.apply(null, a).toFixed(2) : ''
+      case 'avg':
+        a = this.state.data.map((e) => e[`Param${n}`])
+        return a.length ? (a.reduce((a, b) => a + b) / a.length).toFixed(2) : ''
+      default:
+        return ''
+    }
+  }
+
+  getHtmlAgr(n) {
+    return (
+      <div>
+        {this.getAgrData(n)}
+        <select onChange={(e) => this.aggregation(e, n)}>
+          <option value="sum" defaultValue="sum">
+            sum
+          </option>
+          <option value="min">min</option>
+          <option value="max">max</option>
+          <option value="avg">avg</option>
+        </select>
+      </div>
+    )
   }
 
   render() {
@@ -61,6 +106,29 @@ class Main extends Component {
               {this.getRow(e, 20)}
             </div>
           ))}
+          <div className="entity">
+            <div>Итого:</div>
+            {this.getHtmlAgr(1)}
+            {this.getHtmlAgr(2)}
+            {this.getHtmlAgr(3)}
+            {this.getHtmlAgr(4)}
+            {this.getHtmlAgr(5)}
+            {this.getHtmlAgr(6)}
+            {this.getHtmlAgr(7)}
+            {this.getHtmlAgr(8)}
+            {this.getHtmlAgr(9)}
+            {this.getHtmlAgr(10)}
+            {this.getHtmlAgr(11)}
+            {this.getHtmlAgr(12)}
+            {this.getHtmlAgr(13)}
+            {this.getHtmlAgr(14)}
+            {this.getHtmlAgr(15)}
+            {this.getHtmlAgr(16)}
+            {this.getHtmlAgr(17)}
+            {this.getHtmlAgr(18)}
+            {this.getHtmlAgr(19)}
+            {this.getHtmlAgr(20)}
+          </div>
         </div>
       </div>
     )
